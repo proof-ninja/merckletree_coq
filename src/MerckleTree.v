912cb2d70fe611ebf385d736eea4020c701320c6
mube <Yoshihiro Imai>
2019-12-07 13:38:43 +0900

    define verifier

--- src/MerckleTree.v
+++ src/MerckleTree.v
@@ -1,4 +1,5 @@
 Require Import List String.
+Require Import Result.
 Open Scope string_scope.
 
 Variable data hash : Set.
@@ -16,3 +17,38 @@ Inductive MTree : nat -> hash -> mtree -> Prop :=
     MTree n h1 left -> MTree n h2 right ->
     MTree (1 + n) (gen_hash (concat h1 h2)) (N left right)
 .
+
+Fixpoint hash_of_tree t :=
+  match t with
+  | L data => gen_hash data
+  | N left_ right_ =>
+    gen_hash (concat (hash_of_tree left_) (hash_of_tree right_))
+  end.
+
+Definition path := list bool.
+Definition len (p: path) : nat := List.length p.
+
+Inductive proof :=
+| Mk_proof : data -> list hash -> proof.
+
+Definition lenp '(Mk_proof data pstream) := List.length pstream.
+
+Definition p_data '(Mk_proof data pstream) := data.
+Definition p_stream '(Mk_proof data pstream) := pstream.
+
+Fixpoint verifier (path: path) (proof: proof) :=
+  match path with
+  | nil =>
+    Ok (gen_hash (p_data proof))
+  | bit :: path' =>
+    match p_stream proof with
+    | nil => Error ""
+    | hd :: tl =>
+      verifier path' (Mk_proof (p_data proof) tl) >>= fun h' =>
+      if bit then
+        Ok (gen_hash (concat h' hd))
+      else
+        Ok (gen_hash (concat hd h'))
+    end
+  end.
+
