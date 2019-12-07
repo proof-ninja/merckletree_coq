51e4ae71db1a4ea62bccb76ff3118999078e966c
ci-build <ci-build@example.com>
2019-12-07 04:42:19 +0000

    [skip ci] doc upload

--- src/MerckleTree.v
+++ src/MerckleTree.v
@@ -52,3 +52,44 @@ Fixpoint verifier (path: path) (proof: proof) :=
     end
   end.
 
+Fixpoint prover (path:path) (tree : mtree) :=
+  match path with
+  | nil =>
+    match tree with
+    | L data => Ok (Mk_proof data nil)
+    | N _ _ => Error tt
+    end
+  | bit :: path' =>
+    match tree with
+    | L _ => Error tt
+    | N left_ right_ =>
+      if bit then
+        prover path' left_ >>= fun '(Mk_proof data pstream) =>
+        let hr := hash_of_tree right_ in
+        Ok (Mk_proof data (hr :: pstream))
+      else
+        prover path' right_ >>= fun '(Mk_proof data pstream) =>
+        let hl := hash_of_tree left_ in
+        Ok (Mk_proof data (hl :: pstream))
+    end
+  end.
+
+Lemma length_prover : forall path h (tree : mtree) proof,
+    MTree (len path) h tree ->
+    prover path tree = Ok proof ->
+    lenp proof = len path.
+Proof.
+  intros path. induction path; intros h tree proof Htree.
+  - inversion Htree. injection 1. intros Hproof. now subst proof.
+  - inversion Htree. simpl.
+    destruct a.
+    +
+      case_eq (prover path0 left); [|now auto]. intros proof' Hproof'.
+      simpl. destruct proof'. injection 1. intros Hproof. subst.
+      now rewrite <- (IHpath h1 left (Mk_proof d l)).
+    +
+      case_eq (prover path0 right); [|now auto]. intros proof' Hproof'.
+      simpl. destruct proof'. injection 1. intros Hproof. subst.
+      now rewrite <- (IHpath h2 right (Mk_proof d l)).
+Qed.
+
